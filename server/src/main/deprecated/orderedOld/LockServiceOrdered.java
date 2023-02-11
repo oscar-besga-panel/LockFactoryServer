2c4e285903fd890a1c4f3b7ea1865c6397e47a7a
oscar-besga-panel <oscar.besga@panel.es>
2023-02-11 22:42:30 +0100

    old file

--- server/src/main/deprecated/orderedOld/LockServiceOrdered.java
+++ server/src/main/deprecated/orderedOld/LockServiceOrdered.java
@@ -1,5 +1,6 @@
 package org.obapanel.lockfactoryserver.server.service.lock;
 
+import com.google.common.util.concurrent.Futures;
 import org.obapanel.lockfactoryserver.core.LockStatus;
 import org.obapanel.lockfactoryserver.server.LockFactoryConfiguration;
 
@@ -8,18 +9,37 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 import static org.obapanel.lockfactoryserver.server.utils.RuntimeInterruptedException.getToThrowWhenInterrupted;
 
 public class LockServiceOrdered extends LockService {
 
+    private ExecutorService executorService = Executors.newSingleThreadExecutor();
 
     public final ExecutorService executor = Executors.newSingleThreadExecutor();
 
     public LockServiceOrdered(LockFactoryConfiguration configuration) {
         super(configuration);
     }
+
+    //@Override
+    public synchronized String lock2(String name) {
+        String result = super.tryLock(name);
+        if (result == null || result.isEmpty()) {
+            Future<String> future = executorService.submit(() -> lock2(name));
+            try {
+                return future.get();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } catch (ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            return result;
+        }
+    }
+
 
     @Override
     public void shutdown() throws Exception {
