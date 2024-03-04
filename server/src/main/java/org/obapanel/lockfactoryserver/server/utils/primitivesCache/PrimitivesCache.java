ec3f8a60ad7f4e1ee6e560f92d478a91aaed3a62
oscar-besga-panel <oscar.besga@panel.es>
2024-03-04 20:56:27 +0100

    synchonized lock

--- server/src/main/java/org/obapanel/lockfactoryserver/server/utils/primitivesCache/PrimitivesCache.java
+++ server/src/main/java/org/obapanel/lockfactoryserver/server/utils/primitivesCache/PrimitivesCache.java
@@ -7,13 +7,11 @@ import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.DelayQueue;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
@@ -190,11 +188,30 @@ public abstract class PrimitivesCache<K> implements AutoCloseable {
         return dataMap.get(name);
     }
 
+
+    public synchronized void doWithData(String name, Consumer<K> action) {
+        K data = getOrCreateData(name);
+        if (data != null) {
+            action.accept(data);
+        }
+    }
+
+    public synchronized <V> V getWithData(String name, Function<K,V> action) {
+        K data = getOrCreateData(name);
+        if (data != null) {
+            return action.apply(data);
+        } else {
+            return null;
+        }
+    }
+
+
+
     /**
      * Remove data directly
      * @param name name of the primitive to expire
      */
-    public void removeDataIfNotAvoidable(String name) {
+    public synchronized void removeDataIfNotAvoidable(String name) {
         K primitive = dataMap.get(name);
         if (primitive != null) {
             if (avoidExpiration(name, primitive)) {
@@ -216,7 +233,7 @@ public abstract class PrimitivesCache<K> implements AutoCloseable {
      * Remove data directly
      * @param name name of the primitive to expire
      */
-    public void removeData(String name) {
+    public synchronized void removeData(String name) {
         PrimitivesCacheEntry<K> primitive = getQueueEntry(name);
         if (primitive != null) {
             LOGGER.debug("removeData to remove name {} data {}", name, primitive);
@@ -338,7 +355,7 @@ public abstract class PrimitivesCache<K> implements AutoCloseable {
      *
      * @param delayedData data to be checked an removed
      */
-    void removeEntryDataFromQueue(PrimitivesCacheEntry<K> delayedData){
+    synchronized void removeEntryDataFromQueue(PrimitivesCacheEntry<K> delayedData){
         LOGGER.debug("removeEntryDataFromQueue mapName {} delayedData {}", getMapName(), delayedData );
         if (delayedData.isDelayed() ) {
             if (avoidExpiration(delayedData)) {
@@ -375,7 +392,7 @@ public abstract class PrimitivesCache<K> implements AutoCloseable {
     }
 
     /**
-     * Checks for entries in mao not found in queue
+     * Checks for entries in map not found in queue
      * Every entry in map but not in queue is reinserted in queue
      */
     void checkDataEquivalenceInMap() {
